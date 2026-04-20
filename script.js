98f9171abcff97861f54d3738e528709a3098ce0
jsndrsvrgr <joseandres.vergara@gmail.com>
2026-04-19 20:12:05 -0500

    Adding rook functionanility

--- script.js
+++ script.js
@@ -66,8 +66,14 @@ function canPlaceOnRow(row, player) {
 }
 
 function getPieceOwner(piece) {
-  // Do it!
-  return "white"
+  if (piece === "") return null;
+  else if (piece[0] === "W") return "white";
+  else return "black";
+}
+
+function getPieceType(piece){
+  if (piece === "") return null;
+  return piece[1];
 }
 
 function isValidRookMove(fromIndex, toIndex) {
@@ -79,6 +85,38 @@ function isValidRookMove(fromIndex, toIndex) {
 
   return fromRow === toRow || fromCol === toCol;
 }
+
+function isPathClear(fromIndex, toIndex){
+  const fromRow = Math.floor(fromIndex / 8);
+  const fromCol = fromIndex % 8;
+  const toRow = Math.floor(toIndex / 8);
+  const toCol = toIndex % 8;
+
+  let rowStep = 0;
+  let colStep = 0;
+
+  if (toRow > fromRow) rowStep = 1;
+  if (toRow < fromRow) rowStep = -1;
+
+  if (toCol > fromCol) colStep = 1;
+  if (toCol < fromCol) colStep = -1;
+
+  let currentRow = fromRow + rowStep;
+  let currentCol = fromCol + colStep;
+
+  while (currentRow !== toRow || currentCol !== toCol){
+    const checkIndex = currentRow * 8 + currentCol;
+    if (board[checkIndex] !== ""){
+      return false;
+    }
+    currentRow += rowStep;
+    currentCol += colStep;
+  }
+
+  return true;
+ 
+}
+
 
 function renderBoard() {
   boardEl.innerHTML = "";
@@ -101,12 +139,8 @@ function renderBoard() {
     squareEl.addEventListener("click", function () {
 
       const clickedPiece = board[i];
-
-      if (!selectedCard) {
-        statusMessage = "Pick a card first.";
-        renderStatus();
-        return;
-      }
+      
+      //Mode A: a card is selected:
 
       if (selectedCard) {
         if (board[i] !== "") {
@@ -140,6 +174,91 @@ function renderBoard() {
         renderAll();
         return;
       }
+      
+      //MODE B: a rook is already selected:
+
+      if (selectedSquare !== null){
+        const movingPiece = board[selectedSquare];
+
+
+        if (i === selectedSquare){
+          selectedSquare = null;
+          statusMessage = capitalize(currentPlayer);
+          renderAll();
+          return;
+        }
+
+        if (getPieceOwner(movingPiece) !== currentPlayer){
+          statusMessage = "you can only move your own piece"
+          renderStatus();
+          return;
+        }
+        
+        if(getPieceType(movingPiece) !== "R"){
+          statusMessage = "We can only move the rook for now"
+          renderStatus();
+          return;
+        }
+
+        if (clickedPiece !== "" && getPieceOwner(clickedPiece) === currentPlayer){
+          statusMessage = "Rooks move in straight line"
+          renderStatus();
+          return;
+        }
+
+        if (!isPathClear(selectedSquare, i)){
+          statusMessage = "rooks cannot jump over pieces"
+          renderStatus();
+          return;
+        }
+        const playerWhoMoved = currentPlayer;
+        const capturedPiece = board[i];
+
+        board[i] = movingPiece;
+        board[selectedSquare] = "";
+        selectedSquare = null;
+        switchPlayer();
+
+        if (capturedPiece !== ""){
+          statusMessage =
+          capitalize(playerWhoMoved) +
+          " captured" +
+          capturedPiece +
+          ". " +
+          capitalize(currentPlayer) +
+          " turn.";
+    
+        } else{
+          statusMessage = 
+          capitalize(playerWhoMoved) +
+          " moved" +
+          movingPiece +
+          ". " +
+          capitalize(currentPlayer) +
+          " turn."; 
+        }
+        renderAll();
+        return;
+      }
+
+      //Mode C: nothing is selected, so try to select something
+      if (clickedPiece === "") {
+        statusMessage = "Pick a card first.";
+        renderStatus();
+        return;
+      }
+      
+      if (getPieceOwner(clickedPiece) !== currentPlayer){
+        statusMessage = "you can only select you own piece"
+        renderStatus();
+        return;
+      }
+      
+      if(getPieceType(clickedPiece) !== "R"){
+        statusMessage = "For now only rooks can move"
+        renderStatus();
+        return;
+      }
 
       if (board[i] !== "") {
         statusMessage = "That square is already taken.";
@@ -147,11 +266,13 @@ function renderBoard() {
         return;
       }
 
-      board[i] = selectedCard;
       statusMessage = selectedCard + " placed on square " + i + ".";
+      selectedSquare = i;
       selectedCard = null;
+      statusMessage = capitalize(currentPlayer) + " selected rook " + clickedPiece + " ."
       renderAll();
     });
+
 
     boardEl.appendChild(squareEl);
   }
